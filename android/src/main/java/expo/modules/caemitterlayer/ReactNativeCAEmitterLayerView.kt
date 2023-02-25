31416e56c8a2fd4e223ffc2ca69c05c176f1f8cc
DigitalZebra <DigitalZebra@users.noreply.github.com>
2023-02-25 08:30:32 -0600

    initial commit

---
+++ android/src/main/java/expo/modules/caemitterlayer/ReactNativeCAEmitterLayerView.kt
@@ -0,0 +1,7 @@
+package expo.modules.caemitterlayer
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class ReactNativeCAEmitterLayerView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
